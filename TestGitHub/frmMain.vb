6bb932586e07e2dfbb5af5332261fed7e22473a9
Timothy Matthew Allen <timothy.m.allen@outlook.com>
2019-03-14 00:11:47 +0800

    Button2 Added

--- TestGitHub/frmMain.vb
+++ TestGitHub/frmMain.vb
@@ -1,3 +1,9 @@
 ﻿Public Class frmMain
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Hello world")
+    End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox("Another button")
+    End Sub
 End Class
